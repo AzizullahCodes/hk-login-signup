c759be83035d178046cef087108d931728504a04
Azizullah <azizullahcodes@gmail.com>
2026-04-16 12:31:32 +0500

    upd

--- signIn.js
+++ signIn.js
@@ -1,32 +1,80 @@
+let btn = document.getElementById('btn');
+let emailInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
 
-function signIn() {
-    let email = emailInput.value;
-    let password = passwordInput.value;
 
-    // Fix 1: Added 'return' to password validation
-    if (!email) {
-        alert('email is required');
-        return;
-    }
-    if (!password) {
-        alert('password is required');
-        return; 
-    }
-
-    let getData = localStorage.getItem('users') || '[]';
-    let jsonData = JSON.parse(getData);
-
-    let requiredUser = jsonData.find((one) => {
-        return one.email == email && one.password == password;
-    });
-
-    if (requiredUser) {
-        localStorage.setItem('loggedInUserId', requiredUser.id);
-        alert('Login successful!');
-        window.location.href = './home.html';
-    } else {
-        alert('Invalid email or password');
-        // Fix 2: Only clear if login fails
-        clearInput(); 
-    }
+function signIn(){
+   let email = emailInput.value;
+   let password = passwordInput.value;
+//    basic validation 
+if(!email){
+    alert('email is required');
+    return;
 }
+if(!password){
+    alert('password is required')
+}
+// check whether user has an account 
+let getData = localStorage.getItem('users') || '[]';
+console.log(getData);
+let jsonData = JSON.parse(getData);
+let requiredUser = jsonData.find((one)=>{
+    return one.email == email && one.password == password
+})
+
+if(requiredUser){
+    console.log(requiredUser.id)
+    // store only the id of the person who jsut loggedIn
+    localStorage.setItem('loggedInUserId', requiredUser.id)
+    alert('login successfull');
+    
+    window.location.href = './home.html'
+    
+}
+else {
+    alert('Invalid email or password')
+}
+// calling clearInput funciton 
+clearInput()
+}
+
+btn.addEventListener('click',signIn);
+
+// clearInput function 
+function clearInput(){
+    document.getElementById('email').value = '';
+    document.getElementById('password').value = '';
+}
+
+
+// function signIn() {
+//     let email = emailInput.value;
+//     let password = passwordInput.value;
+
+//     // Fix 1: Added 'return' to password validation
+//     if (!email) {
+//         alert('email is required');
+//         return;
+//     }
+//     if (!password) {
+//         alert('password is required');
+//         return; 
+//     }
+
+//     let getData = localStorage.getItem('users') || '[]';
+//     let jsonData = JSON.parse(getData);
+
+//     let requiredUser = jsonData.find((one) => {
+//         return one.email == email && one.password == password;
+//     });
+
+//     if (requiredUser) {
+//         localStorage.setItem('loggedInUserId', requiredUser.id);
+//         alert('Login successful!');
+//         window.location.href = './home.html';
+//     } else {
+//         alert('Invalid email or password');
+//         // Fix 2: Only clear if login fails
+//         clearInput(); 
+//     }
+// }
