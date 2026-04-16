0d6a09849875e929639b22362086af447e0bd161
Azizullah <azizullahcodes@gmail.com>
2026-04-16 12:36:30 +0500

    upd

--- signIn.js
+++ signIn.js
@@ -47,34 +47,3 @@ function clearInput(){
 }
 
 
-// function signIn() {
-//     let email = emailInput.value;
-//     let password = passwordInput.value;
-
-//     // Fix 1: Added 'return' to password validation
-//     if (!email) {
-//         alert('email is required');
-//         return;
-//     }
-//     if (!password) {
-//         alert('password is required');
-//         return; 
-//     }
-
-//     let getData = localStorage.getItem('users') || '[]';
-//     let jsonData = JSON.parse(getData);
-
-//     let requiredUser = jsonData.find((one) => {
-//         return one.email == email && one.password == password;
-//     });
-
-//     if (requiredUser) {
-//         localStorage.setItem('loggedInUserId', requiredUser.id);
-//         alert('Login successful!');
-//         window.location.href = './home.html';
-//     } else {
-//         alert('Invalid email or password');
-//         // Fix 2: Only clear if login fails
-//         clearInput(); 
-//     }
-// }
